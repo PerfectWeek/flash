342020df916d29e2e547201ed85c2da9f27f38e0
alifdev <matthias.alif@epitech.eu>
2019-07-08 12:09:30 +0200

    Add CORS

--- sources/app.ts
+++ sources/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import router from './api/routes/router';
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use(router);
 
