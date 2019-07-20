10250b7cb0766a2951c402f4c1482319007166d3
alifdev <matthias.alif@epitech.eu>
2019-07-20 19:19:09 +0200

    Add expire_at in Room
    Room stay up for an hour

--- sources/models/Room.ts
+++ sources/models/Room.ts
@@ -5,11 +5,13 @@ import { User } from '../utils/User';
 export interface IRoom extends mongoose.Document {
   id: string;
   members: User[];
+  expire_at: Date;
 }
 
 export const roomSchema = new mongoose.Schema({
   id: { type: String, required: true },
   members: { type: Array, required: false },
+  expire_at: { type: Date, default: Date.now, expires: 3600 },
 });
 
 const room = mongoose.model<IRoom>('Room', roomSchema);
