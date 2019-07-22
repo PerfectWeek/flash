3be75c39b582baa86a08277e2ae1d870e398aecd
alifdev <matthias.alif@epitech.eu>
2019-07-22 17:17:05 +0200

    Indent fix

--- sources/api/routes/router.ts
+++ sources/api/routes/router.ts
@@ -13,22 +13,28 @@ const router = Router();
 //
 router.post('/authenticate', expressAsyncHandler(AuthController.googleAuth));
 
-router.get('/authenticate',
-           [check('code').not().isEmpty()],
-           expressAsyncHandler(AuthController.login));
+router.get(
+  '/authenticate',
+  [check('code').not().isEmpty()],
+  expressAsyncHandler(AuthController.login),
+);
 
 //
 // Rooms routes
 //
 router.post('/rooms', expressAsyncHandler(RoomController.createRoom));
 
-router.get('/rooms/:id',
-           [check('id').not().isEmpty()],
-           expressAsyncHandler(RoomController.getRoom));
+router.get(
+  '/rooms/:id',
+  [check('id').not().isEmpty()],
+  expressAsyncHandler(RoomController.getRoom),
+);
 
-router.put('/rooms/:id',
-           [check('id').not().isEmpty()],
-           loggedOnly,
-           expressAsyncHandler(RoomController.joinRoom));
+router.put(
+  '/rooms/:id',
+  [check('id').not().isEmpty()],
+  loggedOnly,
+  expressAsyncHandler(RoomController.joinRoom),
+);
 
 export default router;
