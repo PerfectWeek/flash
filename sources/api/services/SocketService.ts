ec06e4c5a402f162d260a21fbee4b4d192d0325b
alifdev <matthias.alif@epitech.eu>
2019-09-08 15:31:50 +0200

    Broadcast Focused Timeslot infos

--- sources/api/services/SocketService.ts
+++ sources/api/services/SocketService.ts
@@ -4,5 +4,9 @@ export function socketHandler(io: socketIo.Server, socket: socketIo.Socket) {
   socket.on('join', (id: string) => {
     socket.join(id);
     io.to(id).emit('newUser', `New user joined room ${id}`);
+
+    socket.on('focusTimeslot', (start: Date, end: Date) => {
+      io.to(id).emit('timeslotFocus', { start, end });
+    });
   });
 }
