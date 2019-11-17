b741207afd6381bdb838d3feac9eff8f58232b14
alifdev <matthias.alif@epitech.eu>
2019-11-17 11:06:33 +0100

    Fix slot merging bug

--- sources/api/services/SlotFinderService.ts
+++ sources/api/services/SlotFinderService.ts
@@ -1,26 +1,25 @@
-import { TimeSlot } from '../../utils/TimeSlot';
+import { TimeSlot } from "../../utils/TimeSlot";
 
 function overlap(slot: TimeSlot, existingSlot: TimeSlot) {
-  return (slot.startTime.getTime() <= existingSlot.endTime.getTime()
-    && existingSlot.startTime.getTime() <= slot.endTime.getTime());
+  return (
+    slot.startTime.getTime() <= existingSlot.endTime.getTime() &&
+    existingSlot.startTime.getTime() <= slot.endTime.getTime()
+  );
 }
 
-function mergeSingleSlot(slot: TimeSlot, add: TimeSlot) {
-  if (add.startTime < slot.startTime) {
-    slot.startTime = add.startTime;
-  }
-  if (add.endTime > slot.endTime) {
-    slot.endTime = add.endTime;
+function mergeBlock(slots: TimeSlot[], add: TimeSlot) {
+  if (add.endTime > slots[slots.length - 1].endTime) {
+    slots[slots.length - 1].endTime = add.endTime;
   }
 }
 
 function addSlot(mergedSlots: TimeSlot[], slot: TimeSlot) {
-  const block = mergedSlots.find(s => overlap(s, slot));
+  const block = mergedSlots.filter(s => overlap(slot, s));
 
-  if (!block) {
+  if (block.length === 0) {
     mergedSlots.push(slot);
   } else {
-    mergeSingleSlot(block, slot);
+    mergeBlock(block, slot);
   }
 }
 
@@ -32,11 +31,11 @@ function mergeSlots(slots: TimeSlot[]): TimeSlot[] {
 
 export function getOccupiedSlots(
   timeSlots: TimeSlot[],
-  timeWindow: TimeSlot): TimeSlot[] {
-
+  timeWindow: TimeSlot
+): TimeSlot[] {
   const filtered = timeSlots
     .filter(t => overlap(t, timeWindow))
-    .sort((a, b) => a.endTime.getTime() - b.endTime.getTime());
+    .sort((a, b) => a.startTime.getTime() - b.startTime.getTime());
 
   const mergedSlots = mergeSlots(filtered);
   return mergedSlots;
--- sources/api/services/SocketService.ts
+++ sources/api/services/SocketService.ts
@@ -1,12 +1,14 @@
-import socketIo from 'socket.io';
+import socketIo from "socket.io";
 
 export function socketHandler(io: socketIo.Server, socket: socketIo.Socket) {
-  socket.on('join', (id: string) => {
+  socket.on("join", (id: string) => {
     socket.join(id);
-    io.to(id).emit('newUser', `New user joined room ${id}`);
-
-    socket.on('focusTimeslot', (start: Date, end: Date) => {
-      io.to(id).emit('timeslotFocus', { start, end });
-    });
+    io.to(id).emit("newUser");
+  });
+  socket.on("focusTimeslot", (id: string, start: Date, end: Date) => {
+    io.to(id).emit("focusTimeslot", { start, end });
+  });
+  socket.on("disconnect", reason => {
+    console.log(`User disconnected (${reason})`);
   });
 }
