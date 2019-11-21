d8aaf429e432cca80f4a2ae87257c4741bdec83f
alifdev <matthias.alif@epitech.eu>
2019-11-21 19:13:49 +0100

    Update dockerfile

--- dockerfile
+++ dockerfile
@@ -3,6 +3,5 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN npm install
-RUN npm run build
 EXPOSE 3000
-CMD [ "node", "dist/server.js" ]
+CMD [ "npm", "start" ]
--- sources/api/services/SocketService.ts
+++ sources/api/services/SocketService.ts
@@ -5,9 +5,11 @@ export function socketHandler(io: socketIo.Server, socket: socketIo.Socket) {
     socket.join(id);
     io.to(id).emit("newUser");
   });
+
   socket.on("focusTimeslot", (id: string, start: Date, end: Date) => {
     io.to(id).emit("focusTimeslot", { start, end });
   });
+
   socket.on("disconnect", reason => {
     console.log(`User disconnected (${reason})`);
   });
